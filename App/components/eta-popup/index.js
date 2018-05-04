39ebe69b02b0f6a3293ef7878a553bc51de32c90
Johan Erlandsson <erlandd6@gmail.com>
2018-05-04 16:48:10 +0200

    Started adding buttons for timestamps, NOT WORKING

--- App/components/eta-popup/index.js
+++ App/components/eta-popup/index.js
@@ -13,6 +13,7 @@ import {
   ListItem,
   Icon,
   CheckBox,
+  Radiobutton,
   Button
 } from 'react-native-elements';
 import styles from '../../config/styles';
@@ -24,6 +25,14 @@ import { removeFavoriteState, addFavoriteState, } from '../../actions'
 import TopHeader from '../top-header-view';
 import colorScheme from '../../config/colors';
 
+const STAGES = [
+    '72h',
+    '48h',
+    '24h',
+    '12h',
+    '6h',
+];
+
 
 class EtaPopup extends Component {
   state = {
@@ -31,12 +40,18 @@ class EtaPopup extends Component {
     date: "",
     isDatePickerVisible: false,
     isTimePickerVisible: false,
+    checked72: false,
+    checked48: false,
+    checked24: false,
+    checked12: false,
+    checked6: false,
+    checked: true,
   };
-  
+
   _showDatePicker = () => this.setState({ isDatePickerVisible: true });
-  
+
   _hideDatePicker = () => this.setState({ isDatePickerVisible: false });
-  
+
   _handleDatePicked = (date) => {
     console.log(typeof(date));
     console.log(date.getFullYear() + ':' + (date.getMonth()+1) + ':' + date.getDate());
@@ -45,9 +60,9 @@ class EtaPopup extends Component {
   };
 
   _showTimePicker = () => this.setState({ isTimePickerVisible: true });
-  
+
   _hideTimePicker = () => this.setState({ isTimePickerVisible: false });
-  
+
   _handleTimePicked = (date) => {
     console.log('A Time has been picked: ', date.getHours() + " : " + date.getMinutes());
     this.setState({ time: (date.getHours() + ":" + date.getMinutes()) });
@@ -95,7 +110,7 @@ class EtaPopup extends Component {
       onConfirm={this._handleDatePicked}
       onCancel={this._hideDatePicker}
       />
-      
+
       <DateTimePicker
       mode={'time'}
       is24Hour={true}
@@ -103,6 +118,97 @@ class EtaPopup extends Component {
       onConfirm={this._handleTimePicked}
       onCancel={this._hideTimePicker}
       />
+
+      <CheckBox
+      center
+      title='72h'
+      checkedIcon='dot-circle-o'
+      uncheckedIcon='circle-o'
+      checked={this.state.checked72}
+      //onPress={()=>this.setState}
+      onPress={() => {
+        this.setState({checked72: !this.state.checked72})
+        if(checked72 = true){
+          checked48 = false;
+          checked24 = false;
+          checked12 = false;
+          checked6 = false;
+        }}}
+      />
+
+      <CheckBox
+      center
+      title='48h'
+      checkedIcon='dot-circle-o'
+      uncheckedIcon='circle-o'
+      checked={this.state.checked48}
+      onPress={() =>{
+        this.setState({checked48: !this.state.checked48})
+        if(checked48 = true){
+          checked72 = false;
+          checked24 = false;
+          checked12 = false;
+          checked6 = false;
+        }}}
+      />
+
+      />
+
+      <CheckBox
+      center
+      title='24h'
+      checkedIcon='dot-circle-o'
+      uncheckedIcon='circle-o'
+      checked={this.state.checked24}
+      onPress={() =>{
+        this.setState({checked24: !this.state.checked24})
+        if(checked24 == true){
+          checked48 = false;
+          checked72 = false;
+          checked12 = false;
+          checked6 = false;
+        }}}
+      />
+
+      />
+
+      <CheckBox
+      center
+      title='12h'
+      checkedIcon='dot-circle-o'
+      uncheckedIcon='circle-o'
+      checked={this.state.checked12}
+      onPress={() =>{
+        this.setState({checked72: !this.state.checked12})
+        if(checked12 == true){
+          checked48 = false;
+          checked24 = false;
+          checked72 = false;
+          checked6 = false;
+        }}}
+      />
+      />
+
+
+      <CheckBox
+      center
+      title='6h'
+      checkedIcon='dot-circle-o'
+      uncheckedIcon='circle-o'
+      checked={this.state.checked6}
+      onPress={() =>{
+        this.setState({checked6: !this.state.checked6})
+        if(checked6 == true){
+          checked48 = false;
+          checked24 = false;
+          checked12 = false;
+          checked72 = false;
+        }}}
+      />
+      />
+
+
+
       <View style={{ backgroundColor: colorScheme.primaryColor, marginTop: 10, paddingVertical: 5, }}>
       <Button
       title="Confirm"
